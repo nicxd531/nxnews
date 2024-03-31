174419643481d3d8f63f37a3ff5f95fb86bea913
Ola Olasunkanmi <finixd531@gmail.com>
2024-03-31 02:26:53 +0100

    just fixed another mobile isue

--- src/components/reuseable/SearchResult.jsx
+++ src/components/reuseable/SearchResult.jsx
@@ -27,6 +27,7 @@ export default function SearchResult({data,loading,error,setSearching ,width}) {
                     overflow: 'auto',
                     maxHeight: 300,
                     '& ul': { padding: 0 },
+                    pt:{xs:2}
                 }}
                 subheader={<li />}
             >
@@ -34,14 +35,14 @@ export default function SearchResult({data,loading,error,setSearching ,width}) {
                 <Box sx={{ display: "flex", justifyContent: "space-between", flexWrap: "wrap", mt: { xs: 1, lg: 3 } }}>
                         {data?.map((data, index) => {
                             return (
-                                <Box key={index} sx={{ width: { xs: "100%"}, height: { xs: "70px", lg: "100px" }, mb: 1, display: "flex" }}>
+                                <Box key={index} sx={{ width: { xs: "100%"}, height: { xs: "100px", lg: "100px" }, mb: 1, display: "flex" }}>
                                     <Link  href={`/post/${data?._id}/${data?.slug}`}>
                                         <IconButton sx={{textAlign:"start",color:"inherit",p:0,borderRadius:0,height:"100%",width:"100%"}}>
                                             <Box sx={{ width: "30%", height: "100%", objectFit: "cover" }}>
                                                 <img src={data?.mainImage} alt='post image' style={{ width: "100%", height: "100%", objectFit: "cover" }} />
                                             </Box>
                                             <Box sx={{ ml: 2, width: "69%" }} >
-                                                <Typography variant="h5" sx={{ fontSize: { xs: "1rem", lg: "1.3rem" }, fontWeight: "bold", mb: {xs:1,lg:3} }}>{data?.mainHeading}</Typography>
+                                                <Typography variant="h5" sx={{ fontSize: { xs: "0.9rem", lg: "1.3rem" }, fontWeight: "bold", mb: {xs:1,lg:3} }}>{data?.mainHeading}</Typography>
                                                 <Typography sx={{ fontWeight: "bold", fontSize: { xs: "0.7rem", lg: "1rem" } }}>{data?.createdAt}<span className='text-muted'>- 15 minutes ago</span></Typography>
                                             </Box>
                                         </IconButton>
